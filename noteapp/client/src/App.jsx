ea02200390bfd87567bc1244880fcbc8f58104d3
Adem Zaddem <2dem.zaddem@gmail.com>
2026-04-19 12:44:57 +0100

    fixed routes

--- noteapp/client/src/App.jsx
+++ noteapp/client/src/App.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate } from "react-router-dom";
 import { LoginForm } from "./components/login-form";
 import { SignupForm } from "./components/signup-form";
 import LandingPage from "./components/LandingPage";
 import AddNote from "./components/AddNote";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/register" element={<SignupForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/notes" element={<LandingPage />} />
-        <Route path="/notes/add" element = {<AddNote/>}/>
-      </Routes>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<SignupForm />} />
+            <Route path="/notes" element={
+                <ProtectedRoute>
+                    <LandingPage />
+                </ProtectedRoute>
+            } />
+            <Route path="/notes/add" element={
+                <ProtectedRoute>
+                    <AddNote />
+                </ProtectedRoute>
+            } />
+        </Routes>
+    )
 }
 
 export default App;
